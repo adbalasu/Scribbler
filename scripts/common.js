9b645bb7216cfe3a887d8cd96e84d6dfb4816f25
Aditya Balasubramanyam <adbalasu@cisco.com>
2020-07-18 23:46:15 +0530

    Complete Homepage

---
+++ scripts/common.js
@@ -0,0 +1,58 @@
+//SIGNUP
+// Get the modal
+var modal = document.getElementById("signUpModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("signUpButton");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("signup-close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+  console.log("Sign Up Close Clicked");
+}
+
+
+//SIGNIN
+// Get the modal
+var signinModal = document.getElementById("signInModal");
+
+// Get the button that opens the modal
+var signInBtn = document.getElementById("signInButton");
+
+// Get the <span> element that closes the modal
+var signInSpan = document.getElementsByClassName("signin-close")[0];
+
+// When the user clicks on the button, open the modal
+signInBtn.onclick = function() {
+    signInModal.style.display = "block";
+  }
+  
+  // When the user clicks on <span> (x), close the modal
+  signInSpan.onclick = function() {
+    signInModal.style.display = "none";
+    console.log("Sign In Close Clicked");
+  }
+  
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == signInModal || event.target == modal) {
+      signInModal.style.display = "none";
+      modal.style.display = "none";
+      console.log("Clicked outside  Modal");
+    }
+  } 
+
+//NOT A MEMBER SIGNUP
+var signUpHyperLink = document.getElementById("signup-hyperlink");
+
+signUpHyperLink.onclick = function(){
+    signInModal.style.display = "none";
+    modal.style.display = "block";
+}
---
+++ scripts/index.js
@@ -0,0 +1,27 @@
+var allPosts = document.getElementById("allPosts");
+
+allPosts.onclick = function(){
+    window.location = "html/bloglist.html";
+}
+
+//CREATE POST
+// Get the modal
+var createPostModal = document.getElementById("createPostModal");
+
+// Get the button that opens the modal
+var createPostButton = document.getElementById("createPost");
+
+// Get the <span> element that closes the modal
+var createPostCloseSpan = document.getElementsByClassName("create-post-close")[0];
+
+// When the user clicks on the button, open the modal
+createPostButton.onclick = function() {
+  createPostModal.style.display = "block";
+}
+// When the user clicks on <span> (x), close the modal
+createPostCloseSpan .onclick = function() {
+  createPostModal.style.display = "none";
+  console.log("Sign Up Close Clicked");
+}
+
+
